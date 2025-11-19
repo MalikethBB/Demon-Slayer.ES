e6eedb6b302a4b82dd138ab7eb3195657ba02344
Ernest Silins <158049031+MalikethBB@users.noreply.github.com>
2025-11-19 22:48:13 +0000

    Integrate Log4j2 logging pipeline for Person class  (#16)
    * Add Log4j2 logging and Log4j dependency in pom.xml
    
    * Add logging demo to Person class. Create log file on run. Update .gitignore to include .log
    
    * Remove created log file from Repo

--- demonslayer/src/main/java/com/example/demonslayer/App.java
+++ demonslayer/src/main/java/com/example/demonslayer/App.java
@@ -1,13 +1,25 @@
 package com.example.demonslayer;
 
-/**
- * Hello world!
- *
- */
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.Person;
+
 public class App 
 {
+    private static final Logger logger=LogManager.getLogger(App.class);
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        logger.trace("We've just greeted the user!");
+        logger.debug("We've just greeted the user!");
+        logger.info("We've just greeted the user!");
+        logger.warn("We've just greeted the user!");
+        logger.error("We've just greeted the user!");
+        logger.fatal("We've just greeted the user!");
+
+        Person tanjiro = new Person("Tanjiro", 15);
+        Person nezuko = new Person("Nezuko", -5);
     }
 }

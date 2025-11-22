eaf8a57015040f22a7db46c26bd4f792195d30af
Ernest Silins <158049031+MalikethBB@users.noreply.github.com>
2025-11-22 00:25:45 +0000

    Resolved Checkstyle linting issues and improved code style (#21)

--- demonslayer/src/main/java/com/Person.java
+++ demonslayer/src/main/java/com/Person.java
@@ -3,25 +3,58 @@ package com;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Person {
+public final class Person {
+    /** Logger for the Person class. */
+    protected static final Logger LOGGER = LogManager.getLogger(Person.class);
 
-    protected static final Logger logger = LogManager.getLogger(Person.class);
+    /** Name of the person. */
+    private String name;
 
-    protected String name;
-    protected int age;
+    /** Age of the person. */
+    private int age;
 
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
+    /**
+     * Get method for name.
+     *
+     * @return the name of the person
+     */
+    public String getName() {
+        return name;
+    }
 
-        logger.info("Creating a new Person: " + name + " aged " + age);
+    /**
+     * Get method for age.
+     *
+     * @return the age of the person
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Constructs a new Person with a given name and age.
+     *
+     * @param nameParam Name of the person
+     * @param ageParam  Age of the person
+     */
+    public Person(final String nameParam, final int ageParam) {
+        this.name = nameParam;
+        this.age = ageParam;
+
+        LOGGER.info("Creating a new Person: " + name
+                + " aged " + age);
 
         if (age < 0) {
-            logger.error("Age cannot be a negative number. Provided age: " + age);
+            LOGGER.error("Age cannot be a negative number. Provided age: "
+                    + age);
         }
     }
 
+    /**
+     * Introduces the person by printing their name and age.
+     */
     public void intro() {
-        System.out.println("Hey! I'm " + name + " and I'm " + age + " years old.");
+        System.out.println("Hey! I'm " + name
+                + " and I'm " + age + " years old.");
     }
 }
